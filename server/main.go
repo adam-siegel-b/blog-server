38fb5a053632bb0d4379ebb5372cc6d4998672a9
Adam Siegel <adam.siegel@c02dvc27md6m.lan>
2021-12-14 15:38:27 -0800

    changing types

--- server/main.go
+++ server/main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
-
-type Message struct {
-	Code  int
-	Title string
-	Value string
-}
 
 func main() {
 	router := gin.Default()
 	router.GET("/", HelloWorld)
+
+	router.GET("/posts", GetPosts)
+	router.POST("/posts", NewPost)
+	router.PUT("/posts", UpdatePost)
+	router.DELETE("/posts", DeletePost)
 	router.Run(":1337")
-}
-
-// API: curl -X GET http://localhost:1337/
-func HelloWorld(c *gin.Context) {
-
-	hello := Message{200, "Hello", "Hello World!"}
-
-	c.JSON(http.StatusOK, hello)
 }
